4ad71075f601acac80148458db764e923d01c0d1
KARTHIK <vksai17@gmail.com>
2024-05-21 10:46:16 +0530

    Updates

---
+++ precache-manifest.49172f367c129fb1756e7fcdbc27df82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9f122eb9f039788702a199380230ad",
+    "url": "/overReact/index.html"
+  },
+  {
+    "revision": "2edf14a94589972d6acb",
+    "url": "/overReact/static/css/main.662488e1.chunk.css"
+  },
+  {
+    "revision": "e5db2d670436e9f2b3e7",
+    "url": "/overReact/static/js/2.55c97d52.chunk.js"
+  },
+  {
+    "revision": "2edf14a94589972d6acb",
+    "url": "/overReact/static/js/main.e4f8f3fb.chunk.js"
+  },
+  {
+    "revision": "c741bcda682268a9ccb5",
+    "url": "/overReact/static/js/runtime~main.b328f1c1.js"
+  },
+  {
+    "revision": "b98b9440cdb944acae3b80ee58cdbfe7",
+    "url": "/overReact/static/media/search.b98b9440.svg"
+  }
+]);
